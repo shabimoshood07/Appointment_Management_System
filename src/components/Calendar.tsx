576d321e8536faaa47e65564acb3f8d2a5362194
Moshood Olawale <shabimoshood07@gmail.com>
2023-09-04 17:50:47 +0100

    progress of work

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -2,17 +2,43 @@
 
 import { useState } from "react";
 import { Calendar } from "./ui/calendar";
+import AvailableSlotsBtn from "./AvailableSlotsBtn";
+import AddAppointment from "./AddAppointment";
 
 const CalendarComp = () => {
-  const [date, setDate] = useState<Date | undefined>(new Date());
+  const startTime = new Date("Thu Aug 24 2023 08:00:00 GMT+0100");
+  const endTime = new Date("Thu Aug 24 2023 18:00:00 GMT+0100");
+
+  const [date, setDate] = useState<Date>(new Date());
+  const [duration, setDuration] = useState(30);
+  const [dateAppointments, setDateAppointments] = useState([]);
+  const [end, setEnd] = useState<(value: Date) => void>(null);
+  const [start, setStart] = useState<(value: Date) => void>(null);
+
+  //   duration;
+  //   dateAppointments;
+  //   dateStr;
+  //   setEnd;
+  //   setStart;
 
   return (
-    <Calendar
-      mode="single"
-      selected={date}
-      onSelect={setDate}
-      className="rounded-md border shadow bg-slate-300 w-fit"
-    />
+    <>
+      <Calendar
+        mode="single"
+        selected={date}
+        onSelect={setDate}
+        className="rounded-md border shadow bg-slate-300 w-fit"
+      />
+
+      <AvailableSlotsBtn
+        duration={duration}
+        dateStr={date?.toDateString()}
+        dateAppointments={dateAppointments}
+        setEnd={setEnd}
+        setStart={setStart}
+      />
+      {/* <AddAppointment /> */}
+    </>
   );
 };
 
